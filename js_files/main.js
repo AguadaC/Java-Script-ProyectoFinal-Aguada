4a63268b8521fcd213ff2c9e33e2dc5360d42d39
Cristian Aguada <>
2023-09-29 00:24:42 -0300

    feat(DOM/STORAGE): Added style to cards

--- js_files/main.js
+++ js_files/main.js
@@ -11,15 +11,35 @@ function showZones(array){
         let newZoneCard = document.createElement("div")
         newZoneCard.className = "col-12 col-md-6 col-lg-4 my-2"
         newZoneCard.innerHTML = `
-        <div class="card" style="width: 18rem;">
+        <div id="cardToggle${zone.name}" class="card" style="width: 18rem;">
             <img src="./images/${zone.image}" class="card-img-top" alt="image of ${zone.name}">
             <div class="card-body">
             <h5 class="card-title">${zone.name}</h5>
             <p class="card-text">Precio: ${zone.price}</p>
-            <button id="selectTogle${zone.name}" class="btn btn-primary">Add to selection</button>
+            <button id="selectToggle${zone.name}" class="btn btn-primary">Add to selection</button>
             </div>
         </div>`
         validZonesCards.append(newZoneCard)
+
+        const selectToggleBtn = newZoneCard.querySelector(`#selectToggle${zone.name}`);
+        const cardBody = newZoneCard.querySelector(`#cardToggle${zone.name}`);
+        selectToggleBtn.addEventListener("click", () => toggleButtonText(selectToggleBtn, cardBody));
+    }
+}
+
+function toggleButtonText(button, card) {
+    if (button.innerText === "Add to selection") {
+        button.innerText = "Remove from selection";
+        button.classList.remove("btn-primary");
+        button.classList.add("btn-danger");
+        card.classList.remove("border-primary");
+        card.classList.add("border-success");
+    } else {
+        button.innerText = "Add to selection";
+        button.classList.remove("btn-danger");
+        button.classList.add("btn-primary");
+        card.classList.remove("border-success");
+        card.classList.add("border-primary");
     }
 }
 
